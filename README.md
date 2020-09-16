48c1f4a37c119d463d699a12422d6140a864d1ac
rossa <fashionfish@naver.com>
2020-09-16 15:44:07 +0900

    Installing express and configuring babel & nodemon

---
+++ README.md
@@ -0,0 +1,1 @@
+Cloning Yotube With VanillaJS And NodeJS
---
+++ init.js
@@ -0,0 +1,9 @@
+import app from "./app.js";
+
+const PORT = 4000;
+
+const handleListening = () => {
+  console.log(`✅ Listening on: http://localhost:${PORT}`);
+};
+
+app.listen(PORT, handleListening);
