00258f99383d934fef601940e01695cd9fec125a
rossa <fashionfish@naver.com>
2020-10-12 21:43:29 +0900

    Join page layout

---
+++ src/assets/js/form.js
@@ -0,0 +1,61 @@
+const form = document.getElementById("jsForm");
+const inputs = document.querySelectorAll("#jsForm .input");
+
+const handleInput = (e) => {
+    const input = e.target;
+    const inputValue = input.value;
+    const inputContainer = input.parentNode;
+    const title = inputContainer.childNodes[0];
+    const msg = inputContainer.childNodes[1];
+
+    if(inputValue) {
+        input.classList.remove("err");
+        msg.classList.add("hidden");
+        msg.classList.remove("err");
+    } else {
+        input.classList.add("err");
+        msg.innerHTML = "Required";
+        msg.classList.remove("hidden");
+        msg.classList.add("err");
+    }
+}
+
+const handleBlur = (e) => {
+    const input = e.target;
+    const inputValue = input.value;
+    const inputContainer = input.parentNode;
+    const title = inputContainer.childNodes[0];
+    const msg = inputContainer.childNodes[1];
+
+    if(inputValue) {
+        input.classList.remove("err");
+        msg.classList.add("hidden");
+        msg.classList.remove("err");
+    } else {
+        input.classList.add("err");
+        msg.innerHTML = "Required";
+        msg.classList.remove("hidden");
+        msg.classList.add("err");
+    }
+}
+
+const handleFocus = (e) => {
+    const input = e.target;
+    const inputContainer = input.parentNode;
+    const title = inputContainer.childNodes[0];
+    const msg = inputContainer.childNodes[1];
+    
+    title.classList.remove("large");
+}
+
+const init = () => {
+    inputs.forEach((input) => {
+        input.addEventListener("focus", handleFocus);
+        input.addEventListener("blur", handleBlur);
+        input.addEventListener("input", handleInput);
+    })
+}
+
+if (form) {
+    init();
+}
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -4,6 +4,7 @@ import "../scss/style.scss";
 // JS
 import "./loggedUserDropDown";
 import "./commentGenerator";
+import "./form";
 
 const something = () => {
   console.log("Webpack is running");
