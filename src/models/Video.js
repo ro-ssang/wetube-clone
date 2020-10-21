332b6c6f1632385f93cd1887a499f7db9448f5a8
rossa <fashionfish@naver.com>
2020-10-21 23:57:34 +0900

    Setting video schema

---
+++ src/models/Video.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+
+const VideoSchema = new mongoose.Schema({
+  videoFile: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  views: {
+    type: Number,
+    default: 0,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const model = mongoose.model("Video", VideoSchema);
+
+export default model;
